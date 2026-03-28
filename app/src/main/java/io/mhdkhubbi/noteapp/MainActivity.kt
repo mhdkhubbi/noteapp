eb6213ba97c06ad120d771e8a55e786c7e3e2487
mhdkhubbi <mhd.khubbi@gmail.com>
2026-03-28 12:57:11 +0300

    Starting the project Adding all files

---
+++ app/src/main/java/io/mhdkhubbi/noteapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package io.mhdkhubbi.noteapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.mhdkhubbi.noteapp.ui.theme.NoteappTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NoteappTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NoteappTheme {
+
+    }
+}
