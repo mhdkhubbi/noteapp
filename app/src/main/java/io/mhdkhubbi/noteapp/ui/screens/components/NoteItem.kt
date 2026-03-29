5e9db5f3d6f98f093f8820d6345b9426607295ce
mhdkhubbi <mhd.khubbi@gmail.com>
2026-03-29 21:05:19 +0300

    Customizing NoteItem

--- app/src/main/java/io/mhdkhubbi/noteapp/ui/screens/components/NoteItem.kt
+++ app/src/main/java/io/mhdkhubbi/noteapp/ui/screens/components/NoteItem.kt
@@ -1,11 +1,19 @@
 package io.mhdkhubbi.noteapp.ui.screens.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,13 +25,30 @@ import androidx.compose.ui.unit.sp
 fun NoteItem(modifier: Modifier = Modifier) {
 
     Column(Modifier.fillMaxWidth()) {
-        Text("Date of note")
-        Text("Note Title", fontSize = 24.sp, fontWeight = FontWeight.SemiBold)
-        Spacer(Modifier.height(10.dp))
-        Text(
-            "1. ditto skd\n" +
-                    "2. data store shared preferences",fontSize = 16.sp
-        )
+        Card(Modifier.fillMaxWidth(),colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
+        )) {
+            Column(Modifier.fillMaxWidth().padding(20.dp)) {
+                Box(modifier = Modifier
+                    .padding(top = 5.dp)
+
+                    .background(MaterialTheme.colorScheme.secondaryContainer, RoundedCornerShape(5.dp)),
+                    contentAlignment = Alignment.Center
+                ){
+                    Text("Saved On: 1, Jan, 2025", fontSize = 13.sp,modifier= Modifier.padding(start = 5.dp, end = 5.dp))
+                }
+                Spacer(Modifier.height(5.dp))
+                Text("Note Title", fontSize = 24.sp, fontWeight = FontWeight.SemiBold)
+                Spacer(Modifier.height(10.dp))
+                Text(
+                    "1. ditto skd\n" +
+                            "2. data store shared preferences",fontSize = 16.sp,
+
+
+                )
+            }
+        }
+
 
 
     }
