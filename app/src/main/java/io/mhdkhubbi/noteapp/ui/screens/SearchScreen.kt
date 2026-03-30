bec63ce628ede6f231531f640e5d3057514485f5
mhdkhubbi <mhd.khubbi@gmail.com>
2026-03-30 10:29:29 +0300

    Customizing the SearchScreen

--- app/src/main/java/io/mhdkhubbi/noteapp/ui/screens/SearchScreen.kt
+++ app/src/main/java/io/mhdkhubbi/noteapp/ui/screens/SearchScreen.kt
@@ -1,16 +1,23 @@
 package io.mhdkhubbi.noteapp.ui.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import io.mhdkhubbi.noteapp.ui.screens.components.SearchBar
 
 @Composable
 fun SearchScreen(modifier: Modifier = Modifier) {
     Column(modifier.fillMaxSize().padding(start = 20.dp, end = 20.dp,top = 20.dp)) {
+        Text("Search the Archive", fontSize = 34.sp, fontWeight = FontWeight.Medium)
+        Spacer(Modifier.height(10.dp))
         SearchBar()
     }
 
