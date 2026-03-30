93c366dba12da1fecf9faf84d66d6948aa505121
mhdkhubbi <mhd.khubbi@gmail.com>
2026-03-30 12:15:51 +0300

    Adding MainApplication.kt and register it in AndroidManifest.xml

---
+++ app/src/main/java/io/mhdkhubbi/noteapp/MainApplication.kt
@@ -0,0 +1,19 @@
+package io.mhdkhubbi.compose
+
+import android.app.Application
+import io.mhdkhubbi.noteapp.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext.startKoin
+
+class MainApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin{
+            androidLogger()
+            androidContext(this@MainApplication)
+            modules(appModule)
+        }
+    }
+}
